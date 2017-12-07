f2fd13276c4c6b455714d7efa718c245033ce3da
Jeroen van Renen <jpvrenen@hotmail.com>
2017-12-07 15:30:54 +0100

    test1

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+"""Is this a docstring???"""
+RANDOM_DATA = "bladibla"
+
+print "Hello World"
+print "Hello World"
+print "Hello World"
+print "Hello World"
+print "Hello World"
+print "Hello World"
+
+print RANDOM_DATA
